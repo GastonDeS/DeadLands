8e5d89f42045e6015893ef230145f16be4de5cf9
linbrittany <brlin@itba.edu.ar>
2023-05-19 11:47:28 -0300

    fix: enemy cleanup

--- Assets/Scripts/Actors/Characters/Enemy.cs
+++ Assets/Scripts/Actors/Characters/Enemy.cs
@@ -9,36 +9,62 @@ public class Enemy : LifeController
 
     public float AttackCooldown => _attackCooldown;
     [SerializeField] private float _attackCooldown = 1f;
-    private float _currentAttackCooldown = 0;
-    private bool hit = false;
-    private bool isDead = false;
+    
+    private float       _currentAttackCooldown = 0;
+    private bool        _hit = false;
+    private bool        _isDead = false;
+    private GameObject  _mainCharacter;
+    private Animator    _animator;
+    private AudioSource _audioSource;
 
     [SerializeField] private NavMeshAgent agent;
-    private GameObject _mainCharacter;
-
-    private Animator animator;
-    private AudioSource _audioSource;
     [SerializeField] private AudioClip _dyingSound;
     [SerializeField] private AudioClip _hitSound;
 
+    #region UNITY EVENTS
+
     public new void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
-        animator = GetComponentInChildren<Animator>();
+        _audioSource   = GetComponent<AudioSource>();
+        _animator      = GetComponentInChildren<Animator>();
         _mainCharacter = GameObject.FindWithTag("MainCharacter");
+
         base.Start();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (_mainCharacter == null || isDead) return;
+        if (_mainCharacter == null || _isDead) return;
         agent.SetDestination(_mainCharacter.transform.position);
         if (_currentAttackCooldown >= 0)
         {
             _currentAttackCooldown -= Time.deltaTime;
         }
     }
+
+    #endregion
+
+    #region OVERRIDES
+
+    public override void Die() 
+    {
+        Destroy(this.gameObject, 2f);
+        _audioSource.PlayOneShot(_dyingSound);
+        _animator.applyRootMotion = true;
+        _animator.SetBool("death", true);
+        _isDead = true;
+
+        foreach(Collider m_Collider in gameObject.GetComponentsInChildren<Collider>())
+        {
+            m_Collider.enabled = false;
+        } 
+        
+        agent.enabled = false;
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        EventManager.instance.ActionNewKill();
+    }
+
+    #endregion
 
     public void BoostStats(float statsBoost) {
         _damage = (int) (_damage * statsBoost);
@@ -47,42 +73,25 @@ public class Enemy : LifeController
 
     public void OnCollisionEnter(Collision collision)
     {
-        if (hit) return;
-        hit = true;
+        if (_hit) return;
+        _hit = true;
         IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
         Attack(damageable);
-        hit=false;
+        _hit=false;
     }
 
     public void OnCollisionExit(Collision collision)
     {
         IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
-        animator.SetBool("attack", false);
+        _animator.SetBool("attack", false);
     }
-
-    public override void Die() 
-    {
-        Destroy(this.gameObject, 2f);
-        _audioSource.PlayOneShot(_dyingSound);
-        animator.applyRootMotion = true;
-        animator.SetBool("death", true);
-        isDead = true;
-        foreach(Collider m_Collider in gameObject.GetComponentsInChildren<Collider>())
-        {
-            m_Collider.enabled = false;
-        } 
-        agent.enabled = false;
-        gameObject.GetComponent<Rigidbody>().isKinematic = true;
-        EventManager.instance.ActionNewKill();
-    }
-
 
     private void Attack(IDamageable damageable)
     {
         if (_currentAttackCooldown <= 0 && damageable != null)
         {
             damageable.TakeDamage(_damage);
-            animator.SetBool("attack", true);
+            _animator.SetBool("attack", true);
             _audioSource.PlayOneShot(_hitSound);
             _currentAttackCooldown = _attackCooldown;
         }
